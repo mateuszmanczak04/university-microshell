cfb8708d564fd8ccf3c000fb279082245cbd14c5
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 15:29:32 +0100

    Functions

--- microshell.c
+++ microshell.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
-// input using spaces -> fget
-// input not using spaces -> scanf
+void birthday()
+{
+    printf("Happy birthday to you!\n");
+}
 
 int main()
 {
-    char name[25]; // bytes
-    int age;
-
-    printf("What's your name? ");
-    fgets(name, 25, stdin);
-    name[strlen(name)-1] = '\0';
-
-    printf("How old are you? ");
-    scanf("%d", &age);
-
-    printf("Hello %s, how are you?", name);
-    printf("\nYou are %d years old.", age);
-
-    printf("\n");
+    birthday();
 
     return 0;
 }
