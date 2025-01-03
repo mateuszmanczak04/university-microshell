ca3b90d095884cdbb279681534ff3d416d0e3758
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 15:52:19 +0100

    Writing files

--- microshell.c
+++ microshell.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
-void printAge(int *pAge)
-{
-    printf("You are %d years old\n", *pAge);
-    *pAge += 1;
-}
-
 int main()
 {
-    int age = 21;
-    int *pAge = NULL; // Good practice to do it in 2 lines
-    pAge = &age;
+    
+    // FILE *pF = fopen("test.txt", "a");
+    // fprintf(pF, "\nSpongebob Squarepants");
+    // fclose(pF);
 
-    // printf("address of age: %p\n", &age);
-    // printf("address of age: %p\n", pAge);
 
-    // printf("size of age: %d bytes\n", sizeof(age));
-    // printf("size of pAge: %d bytes\n", sizeof(pAge));
+    // if (remove("test.txt") == 0)
+    // {
+    //     printf("That file was deleted successfully\n");
+    // }
+    // else
+    // {
+    //     printf("That file was NOT deleted\n");
+    // }
 
-    // printf("value of age %d\n", age);
-    // printf("value at stored address %d\n", *pAge);
 
-    printAge(&age);
-    printAge(pAge);
 
     return 0;
 }
