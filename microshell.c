fdfb172464b688169000898a973889022269ef72
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:10:58 +0100

    Create playerMove()

--- microshell.c
+++ microshell.c
@@ -23,6 +23,8 @@ int main()
     while (winner == ' ' && checkFreeSpaces() != 0)
     {
         printBoard();
+
+        playerMove();
     }
     return 0;
 }
@@ -66,6 +68,28 @@ int checkFreeSpaces()
 
 void playerMove()
 {
+    int x;
+    int y;
+
+    do
+    {
+        printf("Enter row #(1-3): ");
+        scanf("%d", &x);
+        x--;
+        printf("Enter column #(1-3): ");
+        scanf("%d", &y);
+        y--;
+
+        if (board[x][y] != ' ')
+        {
+            printf("Invalid move!\n");
+        }
+        else
+        {
+            board[x][y] = PLAYER;
+            break;
+        }
+    } while (board[x][y] != ' ');
 }
 
 void computerMove()
