367b1a45df5771e916d8aabf7654780a4d743b6e
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:06:28 +0100

    create printBoard()

--- microshell.c
+++ microshell.c
@@ -19,6 +19,7 @@ int main()
 {
     char winner = ' ';
     resetBoard();
+    printBoard();
     return 0;
 }
 
@@ -35,6 +36,12 @@ void resetBoard()
 
 void printBoard()
 {
+    printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+    printf("\n");
 }
 
 int checkFreeSpaces()
