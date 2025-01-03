c7b0542031383e385d59c9d1b020284291df22b3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:13:23 +0100

    Create checkWinner()

--- microshell.c
+++ microshell.c
@@ -25,6 +25,7 @@ int main()
         printBoard();
 
         playerMove();
+        winner = checkWinner();
     }
     return 0;
 }
@@ -98,6 +99,37 @@ void computerMove()
 
 char checkWinner()
 {
+    // Check rows
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+        {
+            return board[i][0];
+        }
+    }
+
+    // Check columns
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+        {
+            return board[0][i];
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+    {
+        return board[0][0];
+    }
+
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+    {
+        return board[0][2];
+    }
+
+    // No winner
+    return ' ';
 }
 
 void printWinner(char winner)
