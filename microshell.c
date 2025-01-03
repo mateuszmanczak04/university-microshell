68dfd7914e8092c84262eb997e66a8b6f00f7009
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:21:57 +0100

    Clear code

--- microshell.c
+++ microshell.c
@@ -3,22 +3,5 @@
 int main()
 {
 
-    FILE *pF = fopen("./test.txt", "r");
-    char buffer[255];
-
-    if (pF == NULL)
-    {
-        printf("Unable to open file\n");
-    }
-    else
-    {
-        while (fgets(buffer, 255, pF) != NULL)
-        {
-            printf("%s", buffer);
-        }
-    }
-
-    fclose(pF);
-
-    return 0;
+       return 0;
 }
