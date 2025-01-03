6fd9fb3cdc1e02604c7b030fd580f5d0188b5eb4
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:08:12 +0100

    Create checkFreeSpaces() and use it

--- microshell.c
+++ microshell.c
@@ -19,7 +19,11 @@ int main()
 {
     char winner = ' ';
     resetBoard();
-    printBoard();
+
+    while (winner == ' ' && checkFreeSpaces() != 0)
+    {
+        printBoard();
+    }
     return 0;
 }
 
@@ -46,6 +50,18 @@ void printBoard()
 
 int checkFreeSpaces()
 {
+    int freeSpaces = 9;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] != ' ')
+            {
+                freeSpaces--;
+            }
+        }
+    }
+    return freeSpaces;
 }
 
 void playerMove()
