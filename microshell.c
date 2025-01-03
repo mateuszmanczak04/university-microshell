5cc1449ae87388b5bb9c33f4ebb2dde8fb707318
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 16:23:56 +0100

    Add requirements comments

--- microshell.c
+++ microshell.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+/*
+WYMAGANIA:
+2 pkt. - wyświetlać znak zachęty w postaci [{path}] $, gdzie {path} jest ścieżką do bieżącego katalogu roboczego;
+
+2 pkt. - obsługiwać polecenie cd, działające analogicznie jak cd znane nam z powłoki bash;
+
+2 pkt. - obsługiwać polecenie exit, kończące działanie programu powłoki;
+
+2 pkt. - obsługiwać polecenie help, wyświetlające na ekranie informacje o autorze programu i oferowanych przez niego funkcjonalnościach;
+
+6 pkt. - obsługiwać dwa inne, dowolnie wybrane polecenia powłoki (chodzi tutaj np. o własną, samodzielną, średnio zaawansowaną implementację dwóch poleceń, np. cp, których kody nie były podane podczas laboratoriów, nie są rozwiązaniami zadań domowych, ani nie pochodzą ze strony Bartłomieja Przybylskiego); bardzo proste implementacje, takie jak np. echo przy pomocy printf() nie są akceptowane;
+
+6 pkt. (*) - przyjmować polecenia odwołujące się przez nazwę do programów znajdujących się w katalogach opisanych wartością zmiennej środowiskowej PATH oraz umożliwiać wywołanie tych skryptów i programów z argumentami (czyt. fork() + exec*());
+
+4 pkt. - wypisywać komunikat błędu, gdy niemożliwe jest poprawne zinterpretowanie polecenia;
+
+6 pkt. (*) - posiadać tzw. dodatkowe bajery, np. wyświetlanie loginu aktualnie zalogowanego użytkownika, obsługę kolorów, obsługę argumentów w cudzysłowach, sensowną obsługę sygnałów (np. Ctrl+Z), obsługę historii poleceń poprzez strzałki, uzupełnianie składni, itp.; punkty są przyznawane w zależności od stopnia skomplikowania problemu.
+
+*/
+
 int main()
 {
 
-       return 0;
+    return 0;
 }
