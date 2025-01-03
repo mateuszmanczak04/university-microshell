93d45c038e09efbd75649d28d89ce89f4f579cf6
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-03 15:42:57 +0100

    Pointers

--- microshell.c
+++ microshell.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
-void birthday()
+void printAge(int *pAge)
 {
-    printf("Happy birthday to you!\n");
+    printf("You are %d years old\n", *pAge);
+    *pAge += 1;
 }
 
 int main()
 {
-    birthday();
+    int age = 21;
+    int *pAge = NULL; // Good practice to do it in 2 lines
+    pAge = &age;
+
+    // printf("address of age: %p\n", &age);
+    // printf("address of age: %p\n", pAge);
+
+    // printf("size of age: %d bytes\n", sizeof(age));
+    // printf("size of pAge: %d bytes\n", sizeof(pAge));
+
+    // printf("value of age %d\n", age);
+    // printf("value at stored address %d\n", *pAge);
+
+    printAge(&age);
+    printAge(pAge);
 
     return 0;
 }
